d8f3b28833d9009cf1f027646f5804738a3b26f5
Vilenan <vilenan@mail.ru>
2024-10-29 18:20:42 +0300

    add calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,15 @@
 let calculator = {
-  // ваш код
-};
+    read() {
+      this.a = +prompt('Введите a', 0);
+      this.b = +prompt('Введите b', 0);
+    },
+    sum() {
+      return this.a + this.b;
+    },
+    mul(){
+      return this.a * this.b;
+    },
+  };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально

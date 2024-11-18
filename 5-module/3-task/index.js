ab93559bf8162c9c43e4a6ae651a41e7c80cbefc
Vilenan <vilenan@mail.ru>
2024-11-18 11:44:50 +0300

    add carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
-}
+      const arrowLeftEl = document.querySelector('.carousel__arrow_left'),
+        arrowRightEl = document.querySelector('.carousel__arrow_right'),
+        carouselInner = document.querySelector('.carousel__inner'),
+        carouselSlides = document.querySelectorAll('.carousel__slide');
+      const slideWidth =carouselSlides[0].offsetWidth;
+
+      const carouselInnerWidth = carouselInner.offsetWidth;
+      let currentSlide = 0;
+      arrowLeftEl.style.display = 'none';
+
+      function updateCarousel() {
+        carouselInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+        (currentSlide === 0)? arrowLeftEl.style.display = 'none' : arrowLeftEl.style.display = '';
+        (currentSlide === (carouselSlides.length - 1))? arrowRightEl.style.display = 'none' : arrowRightEl.style.display = '';
+      }
+
+
+      arrowRightEl.addEventListener('click', () => {
+        if(currentSlide < carouselSlides.length - 1)
+        currentSlide ++;
+        updateCarousel();
+      })
+
+      arrowLeftEl.addEventListener('click', () => {
+        if(currentSlide > 0)
+        currentSlide --;
+        updateCarousel();
+      })
+
+  }
